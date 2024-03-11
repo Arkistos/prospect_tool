4d0d3ae5993504cf90457883b98e38870d4640a9
Arkistos <pierre.laco@hotmail.fr>
2024-03-11 13:46:12 +0100

    init/setup-mailer

--- src/Controller/EmailGeneratorController.php
+++ src/Controller/EmailGeneratorController.php
@@ -5,13 +5,24 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
 
 class EmailGeneratorController extends AbstractController
 {
     #[Route('/emailgenerator', name: 'app_email_generator')]
-    public function index(): Response
+    public function emailGenerator(MailerInterface $mailer): Response
     {
+
+        $email = (new Email())
+            ->from('me@pierrelacaud.fr')
+            ->to('pierre.lacaud@gmail.com')
+            ->subject('Essai d\'email')
+            ->text('Nouvel Email');
+
+        $mailer->send($email);
+
         return $this->render('emailgenerator.html.twig');
     }
 }
