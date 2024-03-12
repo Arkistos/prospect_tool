c740d31ba306bf910c0950ebe387c330dd92cc2d
Arkistos <pierre.laco@hotmail.fr>
2024-03-12 15:24:36 +0100

    feat/clients dashboard

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Client;
 use App\Form\CSVFormType;
 use App\Service\CSVReader;
 use App\Service\DataManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,27 +15,34 @@ use Symfony\Component\Routing\Attribute\Route;
 class ClientController extends AbstractController
 {
     #[Route('/clients/import', name: 'client_import')]
-    public function cilentImport(Request $request, DataManager $dataManager): Response
+    public function clientImport(Request $request, DataManager $dataManager): Response
     {
-
         $csvForm = $this->createForm(CSVFormType::class);
-
         $csvForm->handleRequest($request);
-
 
         if($csvForm->isSubmitted() && $csvForm->isValid()){
             $pathname = $csvForm['csv']->getData()->getPathName();
             $data = (new CSVReader)->readCSV($pathname);
-
-            $import = $dataManager->storeData($data);
-
-            
+            $dataManager->storeData($data);
         }
-
 
         return $this->render('clients/import.html.twig', [
             'csvForm' => $csvForm,
             'controller_name' => 'ClientController',
         ]);
     }
+
+    #[Route('/clients/dashboard', name:'client_dashboard')]
+    public function clientDashboard(EntityManagerInterface $entityManager):Response
+    {
+
+        $clients = $entityManager->getRepository(Client::class)->findAll();
+
+
+        return $this->render('clients/dashboard.html.twig',[
+            'clients' => $clients,
+            'controller_name' => 'ClientController',
+        ]);
+    }
 }
+
